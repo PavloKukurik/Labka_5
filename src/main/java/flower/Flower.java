01acc0a0281c196e8f07a9689bc91724a0f7526e
PavloKukurik <116553132+PavloKukurik@users.noreply.github.com>
2023-10-12 21:26:54 +0300

    Update Flower.java

--- src/main/java/flower/Flower.java
+++ src/main/java/flower/Flower.java
@@ -1,37 +1,27 @@
 package flower;
+import lombok.Setter;
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@Getter @AllArgsConstructor @ToString @NoArgsConstructor @Setter
 public class Flower {
-    private int price;
+    private FlowerType flowerType;
+    private double price;
+    private double sepalLength;
     private FlowerColor color;
 
-    public int getPrice() {
-        return price;
+    public Flower(Flower flower) {
+        this.flowerType = flower.flowerType;
+        this.price = flower.price;
+        this.sepalLength = flower.sepalLength;
+        this.color = flower.color;
     }
-
-    public void setPrice(int value) {
-        this.price = value;
+    public void setPrice(final int newPrice) {
+        this.price = newPrice;
     }
-
     public String getColor() {
         return color.toString();
-    }
-
-    public void setColor(FlowerColor colour) {
-        this.color = colour;
-    }
-
-    public enum FlowerColor {
-        RED("#FF0000");
-
-        private final String hexCode;
-
-        FlowerColor(String hexCode) {
-            this.hexCode = hexCode;
-        }
-
-        @Override
-        public String toString() {
-            return hexCode;
-        }
     }
 }
